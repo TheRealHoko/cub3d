3519d450582fac2edac16e7377103de2e1c96da4
TheRealHoko <julien.zeybel@live.fr>
2021-02-22 00:57:06 +0100

    minilibx gitignore

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jzeybel <jzeybel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 22:14:21 by jzeybel           #+#    #+#             */
-/*   Updated: 2021/02/21 20:35:36 by jzeybel          ###   ########.fr       */
+/*   Updated: 2021/02/21 20:54:22 by jzeybel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,14 @@ int	main(int ac, char	**av)
 	void	*mlx;
 	void	*window;
 
-	window = NULL;
 	if (ac == 2)
 		res = resparse(av[1]);
 	else
 		return (1);
 	mlx = mlx_init();
+	printf("----------%d-----%d------\n",res[0], res[1]); 
 	window = mlx_new_window(mlx, res[0], res[1], "cub3d");
 	mlx_loop(mlx);
+	mlx_destroy_window(mlx, window);
 	return (0);
 }
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,12 +6,13 @@
 /*   By: jzeybel <jzeybel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 17:06:19 by jzeybel           #+#    #+#             */
-/*   Updated: 2021/02/21 19:54:21 by jzeybel          ###   ########.fr       */
+/*   Updated: 2021/02/21 21:15:12 by jzeybel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 #include "libft.h"
+#include <stdio.h>
 
 int	*resparse(char	*s)
 {
@@ -22,13 +23,14 @@ int	*resparse(char	*s)
 	int			ret;
 
 	fd = open(s, O_RDONLY);
+	ret = get_next_line(fd, &f);
 	while (ret == 1)
 	{
-		if (ft_strchri(f, 0) == 'R')
+		if (ft_strchri(f, 'R') > -1)
 		{
 			line = ft_split(f, ' ');
-			res[0] = ft_atoll(line[0]);
-			res[1] = ft_atoll(line[1]);
+			res[0] = ft_atoll(line[1]);
+			res[1] = ft_atoll(line[2]);
 		}
 		ret = get_next_line(fd, &f);
 	}
